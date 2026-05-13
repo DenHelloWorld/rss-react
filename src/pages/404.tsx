21400e5425093e5125313ecd3ff2ea5f1a041e91
denhelloworld <denissaveliev23@gmail.com>
2026-05-13 06:03:41 +0300

    feat: add 404 image

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,18 @@
 import type { JSX } from 'react';
 import { Link } from 'react-router';
 import { ROUTES } from '../consts/routes.const.ts';
+import LazyImage from '../components/LasyImage.tsx';
 
 const NotFoundPage = (): JSX.Element => {
   return (
     <section className="mx-auto container w-full results-container">
-      <h1 className="subtitle text-left">404 - Page Not Found</h1>
+      <h1 className="subtitle text-left">Page Not Found</h1>
 
       <div className=" text-center flex flex-col items-center gap-4 w-full md:max-w-1/2 mx-auto">
+        <LazyImage
+          src="./404.webp"
+          alt="Sorry, the page you are looking for does not exist"
+        />
         <p className="text-center">
           Sorry, the page you are looking for does not exist.
         </p>
